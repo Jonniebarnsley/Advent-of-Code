4ab88cf05423fdc3161752bd35dbb5b17ee15f2b
Jonniebarnsley <95533733+Jonniebarnsley@users.noreply.github.com>
2022-12-03 16:20:13 +0000

    Add files via upload

--- 2022/day3.py
+++ 2022/day3.py
@@ -1,33 +1,40 @@
+input = '''vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw'''
+
 with open('/Users/jonniebarnsley/Documents/Python/Advent of code/2022/day3.txt', 'r') as file:
     input = file.read()
 
 rucksacks = input.split('\n')
 
-priority = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+priority = '_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-def p1(input):
+def p1(rucksacks):
     ans = 0
     for rucksack in rucksacks:
         l = len(rucksack)
-        c1 = rucksack[:int(l/2)]
-        c2 = rucksack[int(l/2):]
+        c1 = rucksack[:l//2]
+        c2 = rucksack[l//2:]
         for item in c1:
             if item in c2:
-                ans += priority.index(item)+1
+                ans += priority.index(item)
                 break
     return ans
 
-def p2(input):
+def p2(rucksacks):
     ans = 0
-    for n in range(int(len(rucksacks)/3)):
-        elf1 = rucksacks[3*n]
-        elf2 = rucksacks[3*n+1]
-        elf3 = rucksacks[3*n+2]
+    for n in range(0, len(rucksacks), 3):
+        elf1 = rucksacks[n]
+        elf2 = rucksacks[n+1]
+        elf3 = rucksacks[n+2]
         for item in elf1:
             if item in elf2 and item in elf3:
-                ans += priority.index(item)+1
+                ans += priority.index(item)
                 break
     return ans
 
-print(p1(input))
-print(p2(input))
+print(p1(rucksacks))
+print(p2(rucksacks))

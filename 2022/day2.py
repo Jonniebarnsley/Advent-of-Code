1dbfe397aa11923099f8c1cb58df9a07ef862f4e
Jonniebarnsley <95533733+Jonniebarnsley@users.noreply.github.com>
2022-12-03 16:11:36 +0000

    Add files via upload

---
+++ 2022/day2.py
@@ -0,0 +1,32 @@
+input = '''A Y
+B X
+C Z'''
+
+with open('/Users/jonniebarnsley/Documents/Python/Advent of code/2022/day2.txt', 'r') as file:
+    input = file.read()
+
+rounds = [line.split() for line in input.split('\n')]
+
+def score(round):
+    total = 0
+    them, me = round
+    i = 'ABC'.index(them)
+    j = 'XYZ'.index(me)
+    total+=(j-i+1)%3*3
+    total+=j+1
+    return total
+
+def translate(round):
+    them, winlose = round
+    i = 'ABC'.index(them)
+    j = 'XYZ'.index(winlose)
+    me = 'XYZ'[(i+j+2)%3]
+    return (them, me)
+
+p1 = p2 = 0
+for round in rounds:
+    p1 += score(round)
+    p2 += score(translate(round))
+
+print(p1)
+print(p2)
---
+++ 2022/day3.py
@@ -0,0 +1,39 @@
+input = '''vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw'''
+
+with open('/Users/jonniebarnsley/Documents/Python/Advent of code/2022/day3.txt', 'r') as file:
+    input = file.read()
+
+rucksacks = input.split('\n')
+
+priority = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+def p1(input):
+    ans = 0
+    for rucksack in rucksacks:
+        l = len(rucksack)
+        c1 = rucksack[:int(l/2)]
+        c2 = rucksack[int(l/2):]
+        for item in c1:
+            if item in c2:
+                ans += priority.index(item)+1
+                break
+    return ans
+
+def p2(input):
+    ans = 0
+    for n in range(int(len(rucksacks)/3)):
+        elf1 = rucksacks[3*n]
+        elf2 = rucksacks[3*n+1]
+        elf3 = rucksacks[3*n+2]
+        for item in elf1:
+            if item in elf2 and item in elf3:
+                ans += priority.index(item)+1
+                break
+    return ans
+
+print(p2(input))
